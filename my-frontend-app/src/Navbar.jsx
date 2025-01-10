263174994ae70c9da24f87237a900f0d2019a770
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-01-10 20:13:50 +0530

    few changes

--- my-frontend-app/src/Navbar.jsx
+++ my-frontend-app/src/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center px-4 py-3">
         {/* Logo */}
         <a href="/" className="text-2xl font-bold">
-          Sendyy
+          MegaMailer
         </a>
 
         {/* Menu Toggle Button (for mobile view) */}
